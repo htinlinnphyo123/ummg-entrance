813919a71140e2f88e9d6e39d7b451991c032d88
Htin Linn Phyo <htinlinnphyo.npt@gmail.com>
2025-06-23 21:56:55 +0630

    change font color

--- resources/views/components/table/body_column.blade.php
+++ resources/views/components/table/body_column.blade.php
@@ -1,5 +1,5 @@
 @props(['field', 'limit' => 40, 'style' => null, 'image' => null, 'imageStyle' => null])
-<td scope="row" class="px-6 py-4 {{ $style }}">
+<td scope="row" class="px-6 py-4 {{ $style }} text-black font-bold">
     @if ($image)
         <img src="{{ $field }}" alt="" class="{{ $imageStyle }}">
     @else
